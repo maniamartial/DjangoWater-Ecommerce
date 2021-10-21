2eca16d93306dc6e1c3ee745e0250585a82d97d9
martialmania <martialmania19@gmail.com>
2021-10-21 08:51:08 +0300

    deployment modification on settings

--- UmojaWater/settings.py
+++ UmojaWater/settings.py
@@ -16,8 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# warnings.filterwarnings(
-#   "ignore", message="No directory at", module="whitenoise.base")
+warnings.filterwarnings(
+    "ignore", message="No directory at", module="whitenoise.base")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
